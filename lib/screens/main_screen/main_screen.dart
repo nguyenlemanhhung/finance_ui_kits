40365d03448e078550a21b2ce307090a2b71992d
nguyenlemanhhung <github.nohssiw1905@gmail.com>
2022-09-28 16:26:14 +0700

    add new project

--- lib/screens/main_screen/main_screen.dart
+++ lib/screens/main_screen/main_screen.dart
@@ -21,7 +21,7 @@ class _MainScreenState extends State<MainScreen> {
   List pages = [
     HomePage(),
     StatisticPage(),
-    DeleteWidget(),
+    WalletPage(),
     ProfilePage(),
   ];
   @override
--- lib/screens/main_screen/wallet_page/wallet_page.dart
+++ lib/screens/main_screen/wallet_page/wallet_page.dart
@@ -2,46 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
-class DeleteWidget extends StatefulWidget {
-  @override
-  _DeleteWidgetState createState() => _DeleteWidgetState();
-}
+class WalletPage extends StatelessWidget {
+  const WalletPage({super.key});
 
-class _DeleteWidgetState extends State<DeleteWidget> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: ClipPath(
-          clipper: SkewCut(),
-          child: Container(
-            padding: EdgeInsets.symmetric(
-              horizontal: 16,
-              vertical: 22,
-            ),
-            color: Colors.red,
-            width: 200,
-            height: 150,
-            child: Text("Hello World"),
-          ),
-        ),
-      ),
+    return Container(
+      child: Text('wallet page'),
     );
   }
-}
-
-class SkewCut extends CustomClipper<Path> {
-  @override
-  Path getClip(Size size) {
-    final path = Path();
-    path.lineTo(size.width, 0);
-
-    path.lineTo(size.width - 20, size.height);
-    path.lineTo(0, size.height);
-    path.close();
-
-    return path;
-  }
-
-  @override
-  bool shouldReclip(SkewCut oldClipper) => false;
 }
